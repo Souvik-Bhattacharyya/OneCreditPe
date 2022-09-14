3b4ae451f994aa14e384628463b005160eece643
Souvik Bhattacharyya <57744600+Souvik-Bhattacharyya@users.noreply.github.com>
2022-09-14 22:03:04 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+import {
+  Login, 
+  OtpScreen,
+  Home,
+  Loading,
+  Cashbook,
+  CashEntries,
+  ViewReport
+} from './Screens/Pages/index' 
+
+
+const App = () => {
+  return (
+    <>
+      <ViewReport/>
+    </>
+  );
+};
+
+export default App;
---
+++ Screens/Pages/index.js
@@ -0,0 +1,17 @@
+import Login from "./Login/Login";
+import Loading from "./Login/Loading";
+import OtpScreen from "./Login/OtpScreen";
+import Home from "./HomeScreen";
+import Cashbook from "./Cashbook/index";
+import CashEntries from "./Cashbook/CashEntries";
+import ViewReport from "./Cashbook/ViewReport";
+
+export {
+    Login,
+    OtpScreen,
+    Home,
+    Loading,
+    Cashbook,
+    CashEntries,
+    ViewReport
+}
