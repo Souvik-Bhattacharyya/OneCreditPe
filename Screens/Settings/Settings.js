f27dc33afd6f56da35cba61f7feeafeff55a34d9
Souvik Bhattacharyya <57744600+Souvik-Bhattacharyya@users.noreply.github.com>
2022-11-08 18:04:24 +0530

    .

--- Screens/Settings/Settings.js
+++ Screens/Settings/Settings.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -10,9 +10,9 @@ import {
 import Icon from "react-native-vector-icons/FontAwesome5";
 import IconLow from "react-native-vector-icons/FontAwesome";
 import Help from "react-native-vector-icons/Entypo";
-import {List} from "react-native-paper";
+import { List } from "react-native-paper";
 import metrics from "../../Constants/metrics";
-import {useNavigation} from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import CommonHeader from "../../Components/CommonHeader";
 import InviteFriendModal from "../../Components/InviteFriendModal";
 
@@ -23,177 +23,105 @@ const Settings = () => {
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
   const openDialScreen = () => {
-    let number = '';
-    if (Platform.OS === 'ios') {
-      number = 'telprompt:+917980222011';
+    let number = "";
+    if (Platform.OS === "ios") {
+      number = "telprompt:${+917980222011}";
     } else {
-      number = 'tel:+917980222011';
+      number = "tel:${+917980222011}";
     }
     Linking.openURL(number);
   };
 
   return (
-    <>
+    <View style={styles.container}>
       <CommonHeader />
-      <ScrollView>
-        <View style={styles.container}>
-          <View style={{ marginTop: metrics.verticalScale(20), marginBottom: metrics.verticalScale(50) }}>
-            <View style={{
-              width: '100%',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginVertical: 16
+      <View
+        style={{
+          marginTop: metrics.verticalScale(20),
+          marginBottom: metrics.verticalScale(50),
+        }}>
+        <View
+          style={{
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            marginVertical: 16,
+          }}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("UserProfile")}
+            style={{
+              display: "flex",
+              borderColor: "#464555",
+              borderWidth: 3,
+              borderRadius: 50,
+              borderStyle: "dashed",
+              width: 110,
+              height: 110,
+              justifyContent: "center",
+              alignItems: "center",
+              position: "relative",
             }}>
-              <TouchableOpacity
-                onPress={() => navigation.navigate("UserProfile")}
-                style={{
-                  display: 'flex',
-                  borderColor: '#464555',
-                  borderWidth: 3,
-                  borderRadius: 50,
-                  borderStyle: 'dashed',
-                  width: 110,
-                  height: 110,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  position: 'relative'
-                }}>
-                <Image
-                  source={require("../../Assets/profile.png")}
-                  style={{
-                    height: 105,
-                    width: 105,
-                    resizeMode: "contain",
-                    marginVertical: 13,
-                    alignSelf: "center",
-                  }}
-                />
-                <View style={{
-                  width: 36,
-                  height: 36,
-                  backgroundColor: '#464555',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  position: 'absolute',
-                  bottom: -20,
-                  borderRadius: 50,
-                  borderColor: '#fff',
-                  borderWidth: 2
-
-                }}>
-                  <Icon
-                    name="pen"
-                    size={14}
-                    color="#fff"
-                  />
-                </View>
-              </TouchableOpacity>
-            </View>
-            <Text
+            <Image
+              source={require("../../Assets/profile.png")}
               style={{
-                color: "#0A5AC9",
-                fontSize: 20,
-                textAlign: "center",
-                marginTop: 5,
-                fontWeight: '700'
+                height: 105,
+                width: 105,
+                resizeMode: "contain",
+                marginVertical: 13,
+                alignSelf: "center",
+              }}
+            />
+            <View
+              style={{
+                width: 36,
+                height: 36,
+                backgroundColor: "#464555",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "absolute",
+                bottom: -20,
+                borderRadius: 50,
+                borderColor: "#fff",
+                borderWidth: 2,
               }}>
-              Business Name
-            </Text>
-            <Text style={{ textAlign: "center", fontSize: 16, color: '#828282' }}>+91 9192939495</Text>
-          </View>
+              <Icon name="pen" size={14} color="#fff" />
+            </View>
+          </TouchableOpacity>
+        </View>
 
+        <Text
+          style={{
+            color: "#0A5AC9",
+            fontSize: 20,
+            textAlign: "center",
+            marginTop: 5,
+            fontWeight: "700",
+          }}>
+          Business Name
+        </Text>
+        <Text style={{ textAlign: "center", fontSize: 16, color: "#828282" }}>
+          +91 9192939495
+        </Text>
+      </View>
 
-          <List.AccordionGroup>
-            <List.Accordion
-              style={styles.accordion}
-              theme={{ colors: { text: "#464555", primary: '#333' } }}
-              title="Settings"
-              id="1"
-              left={(props) => <Icon name="user-cog" size={20} color={'#464555'} />}
-            >
-              <List.Item title="Language Options"
-                theme={{ colors: { text: "#464555", } }}
-                style={styles.items}
-                onPress={() => navigation.navigate('Language')}
-                left={(props) => <IconLow name="language" size={18} color={'#464555'} style={{ alignSelf: 'center' }} />}
-                right={(props) => <Icon name="arrow-right" size={14} color={'#464555'} style={{ alignSelf: 'center' }} />}
-              />
-              <List.Item title="Log Out"
-                theme={{ colors: { text: "#464555", } }}
-                style={styles.items}
-                onPress={() => { navigation.navigate('login') }}
-                left={(props) => <Help name="log-out" size={18} color={'#464555'} style={{ alignSelf: 'center' }} />}
-                right={(props) => <Icon name="arrow-right" size={14} color={'#464555'} style={{ alignSelf: 'center' }} />}
-              />
-            </List.Accordion>
-
-
-            <List.Accordion
-              style={styles.accordion}
-              theme={{ colors: { text: "#464555", primary: '#333' } }}
-              title="Help & Support"
-              id="2"
-              left={(props) => <Help name="help-with-circle" size={24} color={'#464555'} />}
-            >
-              <List.Item title="FAQ Listing"
-                theme={{ colors: { text: "#464555", } }}
-                style={styles.items}
-                onPress={() => alert("In progress")}
-                right={(props) => <Icon name="arrow-right" size={14} color={'#464555'} style={{ alignSelf: 'center' }} />}
-              />
-              <List.Item title="FAQ Answer"
-                theme={{ colors: { text: "#464555", } }}
-                style={styles.items}
-                onPress={() => alert("In progress")}
-                right={(props) => <Icon name="arrow-right" size={14} color={'#464555'} style={{ alignSelf: 'center' }} />}
-              />
-              <List.Item title="Chat with us"
-                theme={{ colors: { text: "#464555", } }}
-                style={styles.items}
-                onPress={() => Linking.openURL('whatsapp://send?text=Hello&phone=+917980222011')}
-                left={(props) => <Help name="message" size={14} color={'#464555'} style={{ alignSelf: 'center' }} />}
-                right={(props) => <Icon name="arrow-right" size={14} color={'#464555'} style={{ alignSelf: 'center' }} />}
-              />
-              <List.Item title="Call Us"
-                theme={{ colors: { text: "#464555", } }}
-                style={styles.items}
-                onPress={() => openDialScreen()}
-                left={(props) => <Icon name="phone" size={14} color={'#464555'} style={{ alignSelf: 'center' }} />}
-                right={(props) => <Icon name="arrow-right" size={14} color={'#464555'} style={{ alignSelf: 'center' }} />}
-              />
-            </List.Accordion>
-
-
-            <List.Accordion
-              style={styles.accordion}
-              theme={{ colors: { text: "#464555", primary: '#333' } }}
-              title="About Us"
-              id="3"
-              left={(props) => <Icon name="info-circle" size={24} color={'#464555'} />}
-            >
-              <List.Item title="Privacy Policy"
-                theme={{ colors: { text: "#464555", } }}
-                style={styles.items}
-                onPress={() => { navigation.navigate('Privacy And Policy') }}
-                right={(props) => <Icon name="arrow-right" size={14} color={'#464555'} style={{ alignSelf: 'center' }} />}
-              />
-              <List.Item title="Terms And Conditions"
-                theme={{ colors: { text: "#464555", } }}
-                style={styles.items}
-                onPress={() => { navigation.navigate('Terms And Conditions') }}
-                right={(props) => <Icon name="arrow-right" size={14} color={'#464555'} style={{ alignSelf: 'center' }} />}
-              />
+      <List.AccordionGroup>
+        <List.Accordion
+          style={styles.accordion}
+          theme={{ colors: { text: "#464555", primary: "#333" } }}
+          title="Settings"
+          id="1"
+          left={props => <Icon name="user-cog" size={20} color={"#464555"} />}>
           <List.Item
-            title="Invite Friends"
-            theme={{colors: {text: "#464555"}}}
+            title="Language Options"
+            theme={{ colors: { text: "#464555" } }}
             style={styles.items}
-            onPress={showModal}
+            onPress={() => alert("In Progress")}
             left={props => (
-              <Icon
-                name="user-plus"
-                size={14}
+              <IconLow
+                name="language"
+                size={18}
                 color={"#464555"}
-                style={{alignSelf: "center"}}
+                style={{ alignSelf: "center" }}
               />
             )}
             right={props => (
@@ -201,7 +129,179 @@ const Settings = () => {
                 name="arrow-right"
                 size={14}
                 color={"#464555"}
-                style={{alignSelf: "center"}}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+          />
+          <List.Item
+            title="Log Out"
+            theme={{ colors: { text: "#464555" } }}
+            style={styles.items}
+            onPress={() => {
+              navigation.navigate("login");
+            }}
+            left={props => (
+              <Help
+                name="log-out"
+                size={18}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+            right={props => (
+              <Icon
+                name="arrow-right"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+          />
+        </List.Accordion>
+
+        <List.Accordion
+          style={styles.accordion}
+          theme={{ colors: { text: "#464555", primary: "#333" } }}
+          title="Help & Support"
+          id="2"
+          left={props => (
+            <Help name="help-with-circle" size={24} color={"#464555"} />
+          )}>
+          <List.Item
+            title="FAQ Listing"
+            theme={{ colors: { text: "#464555" } }}
+            style={styles.items}
+            onPress={() => navigation.navigate("faq")}
+            right={props => (
+              <Icon
+                name="arrow-right"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+          />
+          <List.Item
+            title="FAQ Answer"
+            theme={{ colors: { text: "#464555" } }}
+            style={styles.items}
+            onPress={() => alert("In progress")}
+            right={props => (
+              <Icon
+                name="arrow-right"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+          />
+          <List.Item
+            title="Chat with us"
+            theme={{ colors: { text: "#464555" } }}
+            style={styles.items}
+            onPress={() =>
+              Linking.openURL("whatsapp://send?text=Hello&phone=+917980222011")
+            }
+            left={props => (
+              <Help
+                name="message"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+            right={props => (
+              <Icon
+                name="arrow-right"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+          />
+          <List.Item
+            title="Call Us"
+            theme={{ colors: { text: "#464555" } }}
+            style={styles.items}
+            onPress={() => openDialScreen()}
+            left={props => (
+              <Icon
+                name="phone"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+            right={props => (
+              <Icon
+                name="arrow-right"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+          />
+        </List.Accordion>
+
+        <List.Accordion
+          style={styles.accordion}
+          theme={{ colors: { text: "#464555", primary: "#333" } }}
+          title="About Us"
+          id="3"
+          left={props => (
+            <Icon name="info-circle" size={24} color={"#464555"} />
+          )}>
+          <List.Item
+            title="Privacy Policy"
+            theme={{ colors: { text: "#464555" } }}
+            style={styles.items}
+            onPress={() => {
+              navigation.navigate("Privacy And Policy");
+            }}
+            right={props => (
+              <Icon
+                name="arrow-right"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+          />
+          <List.Item
+            title="Terms And Conditions"
+            theme={{ colors: { text: "#464555" } }}
+            style={styles.items}
+            onPress={() => {
+              navigation.navigate("Terms And Conditions");
+            }}
+            right={props => (
+              <Icon
+                name="arrow-right"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+          />
+          <List.Item
+            title="Invite Friends"
+            theme={{ colors: { text: "#464555" } }}
+            style={styles.items}
+            onPress={showModal}
+            left={props => (
+              <Icon
+                name="user-plus"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
+              />
+            )}
+            right={props => (
+              <Icon
+                name="arrow-right"
+                size={14}
+                color={"#464555"}
+                style={{ alignSelf: "center" }}
               />
             )}
           />
@@ -210,13 +310,11 @@ const Settings = () => {
             hideModal={hideModal}
             setVisible={setVisible}
           />
-            </List.Accordion>
-          </List.AccordionGroup>
-        </View >
-      </ScrollView>
-    </>
-  )
-}
+        </List.Accordion>
+      </List.AccordionGroup>
+    </View>
+  );
+};
 
 export default Settings;
 
