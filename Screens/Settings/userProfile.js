a8bc335d623d346592cc92540f1a88b792859229
Souvik Bhattacharyya <57744600+Souvik-Bhattacharyya@users.noreply.github.com>
2022-11-13 17:33:52 +0530

    userProfile

--- Screens/Settings/userProfile.js
+++ Screens/Settings/userProfile.js
@@ -5,269 +5,256 @@ import {
   Image,
   TextInput,
   Dimensions,
+  ScrollView,
+  KeyboardAvoidingView
 } from "react-native";
 import React from "react";
 import Icon from "react-native-vector-icons/Entypo";
 import ProfileIcon from "react-native-vector-icons/FontAwesome5";
+import { useDispatch, useSelector } from "react-redux";
+import DocumentPicker, { types } from "react-native-document-picker";
 
-const width = Dimensions.get("window").width;
+
 const UserProfile = () => {
+  const width = Dimensions.get("window").width;
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.auth.user);
+  const [Pic, SetPic] = React.useState('');
+
+  const uploadImage = async () => {
+    try {
+      const response = await DocumentPicker.pick({
+        presentationStyle: 'fullScreen',
+        type: [types.images]
+      });
+
+      SetPic(response);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View
       style={{
         flex: 1,
         backgroundColor: "#fff",
-        paddingTop: 20,
         paddingHorizontal: 15,
+        // justifyContent:'space-between'
       }}>
       <View
         style={{
           width: "100%",
           justifyContent: "center",
           alignItems: "center",
-          marginVertical: 16,
-          flex: 0.2,
+          height: 180
         }}>
-        <TouchableOpacity
-          style={{
-            display: "flex",
-            borderColor: "#464555",
-            borderWidth: 3,
-            borderRadius: 50,
-            borderStyle: "dashed",
-            width: 110,
-            height: 110,
-            justifyContent: "center",
-            alignItems: "center",
-            position: "relative",
-          }}>
-          <Image
-            source={require("../../Assets/blank-profile.png")}
-            style={{
-              height: 105,
-              width: 105,
-              resizeMode: "contain",
-              marginVertical: 13,
-              alignSelf: "center",
-            }}
-          />
+        {Pic.length > 0 ? Pic.map((Is, index) => {
+          return (
+            <View
+              key={index}
+              style={{
+                display: "flex",
+                borderColor: "#464555",
+                borderWidth: 3,
+                borderRadius: 100,
+                borderStyle: "dashed",
+                width: 110,
+                height: 110,
+                justifyContent: "center",
+                alignItems: "center",
+                position: "relative",
+              }}>
+              <Image
+                source={{ uri: Is.uri }}
+                style={{
+                  height: 100,
+                  width: 100,
+                  resizeMode: "cover",
+                  marginVertical: 13,
+                  alignSelf: "center",
+                  borderRadius: 100,
+                }}
+              />
+              <TouchableOpacity
+                onPress={() => uploadImage()}
+                style={{
+                  width: 36,
+                  height: 36,
+                  backgroundColor: "#464555",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  position: "absolute",
+                  bottom: -20,
+                  borderRadius: 50,
+                  borderColor: "#fff",
+                  borderWidth: 2,
+                }}>
+                <Icon name="camera" size={18} color="#fff" />
+              </TouchableOpacity>
+            </View>
+          )
+        })
+          :
           <View
             style={{
-              width: 36,
-              height: 36,
-              backgroundColor: "#464555",
+              display: "flex",
+              borderColor: "#464555",
+              borderWidth: 3,
+              borderRadius: 50,
+              borderStyle: "dashed",
+              width: 110,
+              height: 110,
               justifyContent: "center",
               alignItems: "center",
-              position: "absolute",
-              bottom: -20,
-              borderRadius: 50,
-              borderColor: "#fff",
-              borderWidth: 2,
+              position: "relative",
             }}>
-            <Icon name="camera" size={14} color="#fff" />
+            <Image
+              source={require("../../Assets/blank-profile.png")}
+              style={{
+                height: 105,
+                width: 105,
+                resizeMode: "contain",
+                marginVertical: 13,
+                alignSelf: "center",
+              }}
+            />
+            <TouchableOpacity
+              onPress={() => uploadImage()}
+              style={{
+                width: 36,
+                height: 36,
+                backgroundColor: "#464555",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "absolute",
+                bottom: -20,
+                borderRadius: 50,
+                borderColor: "#fff",
+                borderWidth: 2,
+              }}>
+              <Icon name="camera" size={18} color="#fff" />
+            </TouchableOpacity>
           </View>
-        </TouchableOpacity>
+        }
       </View>
-
-      <View style={{flex: 1}}>
-        <View
-          style={{
-            flexDirection: "row",
-            width: "100%",
-            alignItems: "center",
-            borderColor: "#c9c9c9",
-            borderBottomWidth: 1,
-            paddingHorizontal: 10,
-            paddingVertical: 10,
-          }}>
-          <ProfileIcon
-            name="user-alt"
-            color={"#464555"}
-            size={20}
-            style={{marginRight: 10, marginRight: 20}}
-          />
-          <TextInput
-            placeholder="Your Name"
-            placeholderTextColor={"#aaa"}
-            style={{
-              color: "#464555",
-              fontSize: 18,
-              fontWeight: "500",
-              width: "100%",
-            }}
-          />
-
-          <TouchableOpacity
-            style={{
-              position: "absolute",
-              right: 20,
-              borderRadius: 6,
-              flexDirection: "row",
-              justifyContent: "center",
-              display: "none",
-            }}>
-            <Text
+      <ScrollView>
+        <View style={{
+          // backgroundColor: '#f6f6f6',
+          marginVertical: 20,
+          height: 250,
+          justifyContent: 'flex-start'
+        }}>
+          <KeyboardAvoidingView>
+            <View
               style={{
-                color: "#333",
-                fontSize: 14,
-                fontWeight: "900",
-                color: "#0a5ac9",
+                flexDirection: "row",
+                width: "100%",
+                alignItems: "center",
+                borderColor: "#c9c9c9",
+                borderBottomWidth: 1,
+                paddingHorizontal: 10,
+                paddingVertical: 10,
               }}>
-              Save
-            </Text>
-          </TouchableOpacity>
-        </View>
+              <ProfileIcon
+                name="user-alt"
+                color={"#464555"}
+                size={20}
+                style={{ marginRight: 10, marginRight: 20 }}
+              />
+              <TextInput
+                placeholder="Your Name"
+                placeholderTextColor={"#aaa"}
+                style={{
+                  color: "#464555",
+                  fontSize: 18,
+                  fontWeight: "500",
+                  width: "100%",
+                }}
+              />
+            </View>
 
-        <View
-          style={{
-            flexDirection: "row",
-            width: "100%",
-            alignItems: "center",
-            borderColor: "#c9c9c9",
-            borderBottomWidth: 1,
-            paddingHorizontal: 10,
-            paddingVertical: 10,
-            // marginTop: 10
-          }}>
-          <ProfileIcon
-            name="phone"
-            color={"#464555"}
-            size={20}
-            style={{marginRight: 10, marginRight: 20}}
-          />
-          <TextInput
-            value="810123523"
-            placeholderTextColor={"#aaa"}
-            style={{
-              color: "#464555",
-              fontSize: 18,
-              fontWeight: "500",
-              width: "100%",
-            }}
-          />
-
-          <TouchableOpacity
-            style={{
-              position: "absolute",
-              right: 20,
-              borderRadius: 6,
-              flexDirection: "row",
-              justifyContent: "center",
-              // display: 'none'
-            }}>
-            <Text
+            <View
               style={{
-                color: "#333",
-                fontSize: 14,
-                fontWeight: "900",
-                color: "#0a5ac9",
+                flexDirection: "row",
+                width: "100%",
+                alignItems: "center",
+                borderColor: "#c9c9c9",
+                borderBottomWidth: 1,
+                paddingHorizontal: 10,
+                paddingVertical: 10,
+                // marginTop: 10
               }}>
-              Save
-            </Text>
-          </TouchableOpacity>
-        </View>
+              <ProfileIcon
+                name="phone"
+                color={"#464555"}
+                size={20}
+                style={{ marginRight: 10, marginRight: 20 }}
+              />
+              <TextInput
+                value={"+91 " + user.mobile}
+                placeholderTextColor={"#aaa"}
+                style={{
+                  color: "#464555",
+                  fontSize: 18,
+                  fontWeight: "500",
+                  width: "100%",
+                }}
+              />
 
-        <View
-          style={{
-            flexDirection: "row",
-            width: "100%",
-            alignItems: "center",
-            borderColor: "#c9c9c9",
-            borderBottomWidth: 1,
-            paddingHorizontal: 10,
-            paddingVertical: 10,
-            // marginTop: 10
-          }}>
-          <ProfileIcon
-            name="building"
-            color={"#464555"}
-            size={20}
-            style={{marginRight: 10, marginRight: 20}}
-          />
-          <TextInput
-            placeholder="Your Name"
-            value="Business Name"
-            placeholderTextColor={"#aaa"}
-            style={{
-              color: "#464555",
-              fontSize: 18,
-              fontWeight: "500",
-              width: "100%",
-            }}
-          />
+            </View>
 
-          <TouchableOpacity
-            style={{
-              position: "absolute",
-              right: 20,
-              borderRadius: 6,
-              flexDirection: "row",
-              justifyContent: "center",
-              display: "none",
-            }}>
-            <Text
+            <View
               style={{
-                color: "#333",
-                fontSize: 14,
-                fontWeight: "900",
-                color: "#0a5ac9",
+                flexDirection: "row",
+                width: "100%",
+                alignItems: "center",
+                borderColor: "#c9c9c9",
+                borderBottomWidth: 1,
+                paddingHorizontal: 10,
+                paddingVertical: 10,
+                // marginTop: 10
               }}>
-              Save
-            </Text>
-          </TouchableOpacity>
+              <ProfileIcon
+                name="building"
+                color={"#464555"}
+                size={20}
+                style={{ marginRight: 10, marginRight: 20 }}
+              />
+              <TextInput
+                placeholder="Your Name"
+                value={user.name}
+                placeholderTextColor={"#aaa"}
+                style={{
+                  color: "#464555",
+                  fontSize: 18,
+                  fontWeight: "500",
+                  width: "100%",
+                }}
+              />
+            </View>
+          </KeyboardAvoidingView>
         </View>
-
-        <View
-          style={{
-            flexDirection: "row",
-            width: "100%",
-            alignItems: "center",
-            borderColor: "#c9c9c9",
-            borderBottomWidth: 1,
-            paddingHorizontal: 10,
-            paddingVertical: 10,
-            // marginTop: 10
-          }}>
-          <ProfileIcon
-            name="users"
-            color={"#464555"}
-            size={20}
-            style={{marginRight: 10, marginRight: 20}}
-          />
-          <TextInput
-            placeholder="Stuff Count"
-            value=""
-            placeholderTextColor={"#aaa"}
-            style={{
-              color: "#464555",
-              fontSize: 18,
-              fontWeight: "500",
-              width: "100%",
-            }}
-          />
-
-          <TouchableOpacity
-            style={{
-              position: "absolute",
-              right: 20,
-              borderRadius: 6,
-              flexDirection: "row",
-              justifyContent: "center",
-              display: "none",
-            }}>
-            <Text
-              style={{
-                color: "#333",
-                fontSize: 14,
-                fontWeight: "900",
-                color: "#0a5ac9",
-              }}>
-              Save
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
+      </ScrollView>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#0a5ac9',
+          paddingVertical: 15,
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 6,
+          marginBottom: 10,
+          marginTop: 'auto'
+        }}>
+        <Text style={{
+          color: '#fff',
+          fontSize: 14
+        }}>
+          Save
+        </Text>
+      </TouchableOpacity>
+    </View >
   );
 };
 
